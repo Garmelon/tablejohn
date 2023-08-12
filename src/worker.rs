81328fcf041977e8ad5d26bc3a648cd04554da96
Joscha <joscha@plugh.de>
2023-08-12 15:57:23 +0200

    Start reimplementing worker

--- src/worker.rs
+++ src/worker.rs
@@ -1,16 +1,6 @@
-mod coordinator;
-mod run;
-mod server;
-mod tree;
+use tracing::error;
 
-use std::sync::{Arc, Mutex};
-
-use tokio::task::JoinSet;
-use tracing::{debug, error};
-
-use crate::config::{Config, WorkerServerConfig};
-
-use self::{coordinator::Coordinator, server::Server};
+use crate::config::Config;
 
 pub struct Worker {
     config: &'static Config,
@@ -27,30 +17,6 @@ impl Worker {
             return;
         }
 
-        let coordinator = Arc::new(Mutex::new(Coordinator::new()));
-
-        let mut tasks = JoinSet::new();
-        for (name, server_config) in self.config.worker_servers.iter() {
-            debug!("Launching task for server {name}");
-            let mut server = Server::new(
-                name.clone(),
-                self.config,
-                server_config,
-                coordinator.clone(),
-            );
-            tasks.spawn(async move { server.run().await });
-        }
-
-        while tasks.join_next().await.is_some() {}
+        todo!()
     }
-}
-
-pub fn launch_standalone_server_task(
-    config: &'static Config,
-    server_name: String,
-    server_config: &'static WorkerServerConfig,
-) {
-    let coordinator = Arc::new(Mutex::new(Coordinator::new()));
-    let mut server = Server::new(server_name, config, server_config, coordinator);
-    tokio::task::spawn(async move { server.run().await });
 }
